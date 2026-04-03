604960d8d91961dd58a0d39773d4abb1b86b9cd8
nhyirasamuel <nhyirasamuel4@gmail.com>
2026-04-03 05:25:15 +0000

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found. Make sure index.html has a <div id='root'>.");
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
